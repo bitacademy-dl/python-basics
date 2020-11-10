b50b4c3cbbd14bc7e4a1d80496bb3b3fa29bce8b
bitacademy-dl <bitacademy.dl@gmail.com>
2020-11-10 17:16:39 +0900

    비트연산자 연습

---
+++ 03/operator-bitwise.py
@@ -0,0 +1,17 @@
+# ~
+print(~5)
+print(~-1)
+
+# << 연산자
+a = 4
+print(a >> 1)
+
+# and(&) :  True and False -> False  1 & 0 -> 0
+# or(|)  :  True or False -> True    1 | 0 -> 1
+# XOR(^) :  True xor False -> True   1 ^ 0 -> 1
+a = 3
+print(a & 2)
+print(a | 8)
+print(0x0f ^ 0x06)
+
+
---
+++ 03/str.py
@@ -0,0 +1,12 @@
+# 한줄 문자열 리터럴
+s1 = 'Hello World'
+
+print(s1)
+
+# 여러줄 문자열 리터럴
+# s2 = 'Hello\nworld'
+s2 = """Hello
+world
+"""
+print(s2)
+
