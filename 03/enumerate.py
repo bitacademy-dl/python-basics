384aa9839b5981a7094cf8df20b01641d2f2c593
bitacademy-dl <bitacademy.dl@gmail.com>
2020-11-12 15:17:58 +0900

    enumerate

--- 03/enumerate.py
+++ 03/enumerate.py
@@ -1,1 +1,10 @@
 # enumerate
+
+index = 1
+for color in ['red', 'blue', 'yellow', 'gray']:
+    print(f'{index}:{color}')
+    index += 1
+
+
+for i, color in enumerate(['red', 'blue', 'yellow', 'gray']):
+    print(f'{i+1}:{color}')
